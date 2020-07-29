7fb188839736947eddc4865a3d29efe973bb4f92
fmalik-crypto <55195792+fmalik-crypto@users.noreply.github.com>
2020-07-29 18:10:43 +0300

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,1 +1,16 @@
-#this file contains the code to generate plot2.png using data set "Electric power consumption"
+#this file contains the code to generate plot1.png using data set "Electric power consumption"
+
+# read the dataset "Electric power consumption"
+mydata <- read.table('data.txt', sep=';', header=TRUE)
+
+# subset the dataset by converting the Date from char to date format and then filtering on Feb 01 and 02, 2007 data
+mydata %>% mutate(Date = strptime(Date, %d%m%Y) %>% filter(Date == as.Date("02/01/2007", %d%m%Y) | Date == as.Date("02/02/2007", %d%m%Y))
+
+# create plot in png file                                
+png(filename='plot2.png', width=480, height=480, units='px')
+
+# plot the filtered Global_active_power 
+plot(mydata$Time, mydata$Global_active_power, type="l", xlab="", ylab="Global Active Power (in kilowatts)")
+
+# close the file
+dev.off()
